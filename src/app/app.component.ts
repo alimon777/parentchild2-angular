58642972b5f8c542370368b13f4a993fe2256f18
alimon777 <alimonna777@gmail.com>
2024-10-28 17:25:15 +0530

    child emits and parent receives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'parentchild2';
+
+  childItems: string[] = []; 
+
+  childEvent(childItem: string) {
+    this.childItems.push(childItem); 
+  }
 }
